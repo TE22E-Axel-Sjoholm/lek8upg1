2a91eb899c7af141de425f1911e6bb1629be11f3
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-11-13 09:00:45 +0100

    D

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int i=0; i<6; i++) {
+        for (int i=2; i<6; i++) {
             System.err.println(i);
         }
     }
