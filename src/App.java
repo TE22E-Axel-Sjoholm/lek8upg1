49666e487db3051e21fc7148e97ad1ffc0e6b1dc
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-11-13 08:59:29 +0100

    upg1 B

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-            int i = 0;
+            int i = 2;
 
         while(i<=5){
             System.out.println(i);
