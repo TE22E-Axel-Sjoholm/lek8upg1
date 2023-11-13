a19e39c1eceb0c82270239a40faf8618a88c5de9
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-11-13 09:00:33 +0100

    C

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-            int i = 2;
-
-        while(i<=5){
-            System.out.println(i);
-            i++;
+        for (int i=0; i<6; i++) {
+            System.err.println(i);
         }
     }
 }
